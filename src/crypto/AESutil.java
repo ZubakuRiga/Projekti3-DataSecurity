7a818b73f4ff129605a8e4f888b9b338c6beae8b
RinesaSaraci <rinesasaraci1@gmail.com>
2025-05-18 16:52:19 +0200

    implemented AES key generation, encryption and decryption utilities

---
+++ src/crypto/AESutil.java
@@ -0,0 +1,34 @@
+package crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESutil {
+    public static SecretKey generateAESKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        return keyGen.generateKey();
+    }
+
+    public static String encryptAES(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+    }
+
+
+    public static String decryptAES(String encrypted, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encrypted)));
+    }
+
+    public static SecretKey decodeAESKey(String encodedKey) {
+        byte[] decoded = Base64.getDecoder().decode(encodedKey);
+        return new SecretKeySpec(decoded, 0, decoded.length, "AES");
+    }
+
+}
